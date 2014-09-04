6150eb6792af29fca0661c619ed8977e3832ed04
Damian Rizzotto <zeitzendesign@gmail.com>
2014-09-04 19:43:33 -0300

    no message

---
+++ IncredibleMachine/IncredibleMachine/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  IncredibleMachine
+//
+//  Created by Damian Rizzotto on 04/09/14.
+//  Copyright El Tucu games 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ IncredibleMachine/IncredibleMachine/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IncredibleMachine
+//
+//  Created by Damian Rizzotto on 04/09/14.
+//  Copyright El Tucu games 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
