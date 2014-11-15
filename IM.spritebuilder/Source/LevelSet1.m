f10c3b1a9de2f3e21d5425778c35bfcf8a62d5a1
Damian Rizzotto <zeitzendesign@gmail.com>
2014-11-15 15:28:20 -0300

    dfdffgdf

---
+++ IM.spritebuilder/Source/LevelSet1.m
@@ -0,0 +1,21 @@
+//
+//  LevelSet1.m
+//  IM
+//
+//  Created by Damian Rizzotto on 06/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "LevelSet1.h"
+
+
+@implementation LevelSet1
+
+-(id)init{
+    super.setName = @"set1";
+    super.setNum = 1;
+    
+    return [super init];
+}
+
+@end
